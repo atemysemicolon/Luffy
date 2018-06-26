ae782f3eee1923baf383e2952b6b5baf515b3162
ALTRAN-Services, Prassanna Ganesh R <altran-services.prassanna.ganesh.ravishankar@hp.com>
2018-06-26 19:16:18 +0200

    Parses the complaint section and dumps it to the log [TODO] Load the url and re-search within [TODO] Display results with popup.html

--- content.js
+++ content.js
@@ -2,8 +2,24 @@
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     // If the received message has the expected format...
     if (msg.text === 'report_back') {
+
+    	console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
+    	console.log(document.all[0].outerHTML.slice(0,200));
+
+    	//Make an element tree out of the html
+    	var html = document.createElement('html')
+    	html.innerHTML = document.all[0].outerHTML.trim();
+    	console.log("Firrst child is" + html.firstChild);
+
+    	var html_jquery = $($.parseHTML(document.all[0].outerHTML));
+		var links = html_jquery.find("#botstuff").find("a").attr("href");
+		console.log(links)
+    	
+ 
+
+
         // Call the specified callback, passing
         // the web-page's DOM content as argument
-        sendResponse(document.all[0].outerHTML);
+        //sendResponse(document.all[0].outerHTML);
     }
 });
